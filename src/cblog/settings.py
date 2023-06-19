68b28cb0388ab236cd0efbd20627b26253509982
Latif Yildirim <113920287+latifyildirim@users.noreply.github.com>
2023-06-19 13:44:24 +0200

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'database-11.c33ndfedld89.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'database-1.c33ndfedld89.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
